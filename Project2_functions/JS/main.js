01096aeee0c7954800af841a857d8a045036136c
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2022-05-27 17:22:29 +0100

    Assignmet #2

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+ function MyFunc() {
+
+         var a = "This text is red";
+                a += " and I like it"
+         var result = a.fontcolor("red");
+         document.getElementById("greent").innerHTML=result;
+     }
+
