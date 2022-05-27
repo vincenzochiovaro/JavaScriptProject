e4bd7bd5c782a79044d258eecdf6dbc5fc37d883
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2022-05-27 16:19:12 +0100

    Assignment 1#

---
+++ Project1_expressions_alert/Js/main.js
@@ -0,0 +1,10 @@
+window.alert("Hello World");
+
+
+var a = "first";   //assign a string to a variable
+var a = a.fontcolor("red"); // assign color to  a
+
+var b = "second";   //assign a string to a viariable
+
+
+document.write(a+b); //  in this expression print the result of var a and b 
