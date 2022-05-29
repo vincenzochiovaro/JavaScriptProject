b26d2bf69e544372c793c1e5b187d950ab289641
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2022-05-29 11:24:38 +0100

    Ternary,Operators,Constructors

---
+++ Project6_ternary_operators_constructors/JS/main.js
@@ -0,0 +1,77 @@
+
+// TERNARY
+
+
+
+// function Vote_Function() {
+//        var Age, Can_ride;
+//        Age=document.getElementById("Age").value;
+//        system = (Age < 18)? "You are too young":"Congrats you can ";
+//        document.getElementById("enter").innerHTML = system + " vote.";
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+// END TERNARY
+
+
+
+
+// CONSTRUCTOR FUNCTION NEW AND THIS
+
+
+// function Vehicle (Make, Model, Year, Color) {
+//               this.Vehicle_Make = Make;
+//               this.Vehicle_Model = Model;
+//               this.Vehicle_Year = Year;
+//               this.Vehicle_Color = Color;
+// }
+
+// var Jack = new Vehicle ("Dodge", "Viper", 2020, "Red");
+// var Emily = new Vehicle ("Jeep", "Trail", 2019, "White");
+// var Erik = new Vehicle ("Ford", "Pinto", 1971, "Mustard");
+// function myFunction() {
+//        document.getElementById("Keywords_and_Constructors").innerHTML = 
+//        "Erik drives a" + Erik.Vehicle_Color+"-colored"+ Erik.Vehicle_Model+
+//        "manufactured in " + Erik.Vehicle_Year;
+
+// }
+
+
+
+
+
+// END CONSTRUCTOR FUNCTION NEW AND THIS
+
+
+
+
+
+
+// NESTED FUNCTION 
+
+
+// function Nested_Function() {
+//        document.getElementById("Nest").innerHTML= Count();
+//        function Count() {
+//               var Starting_point = 9;
+//               function Plus_one() {Starting_point += 1;}
+//               Plus_one();
+//               return Starting_point;
+//        }
+// } 
+
+
+
+
+
+// END NESTED FUNCTION
