0321e4d6ee269fa62dd687c7e1d82ce311cb21a8
Vincenzo Chiovaro <vincenzochiovaro194@gmail.com>
2022-05-30 16:02:03 +0100

    methods

---
+++ Project8_string_methods/JS/main.js
@@ -0,0 +1,100 @@
+// function full_sentence () {
+//     var part1 = "I Have ";
+//     var part2 = " made this ";
+//     var part3 = " into a complete ";
+//     var part4 = " sentence ";
+//     var whole_sentence = part1.concat(part2, part3, part4);
+//     document.getElementById("Concatenate").innerHTML = whole_sentence;
+
+// }
+
+// END CONCAT
+
+
+
+
+
+
+// SLICE 
+
+
+
+// function slice_method() {
+//     var sentence = "All work and no play makes Jhonny a dull boy";
+//     var section = sentence.slice(27,33); //position 27 to 33 position start from 0
+//     document.getElementById("slice").innerHTML = section;
+
+// }
+
+// END SLICE
+
+
+
+
+
+
+
+
+
+
+ 
+// UPPER CASE
+
+
+
+// str = new String("hi");
+
+// console.log(str)
+// console.log(str.toUpperCase());
+
+// END UPPER CASE 
+
+
+
+
+
+
+//search method 
+
+
+// var str = "the sky is blue"
+
+// var blue = "blue"
+// console.log (str.search(blue));
+
+
+//end search method
+
+
+
+
+
+// toFixed
+
+// var number = 158.12345
+// console.log(number);
+// console.log(number.toFixed(2));  // it will stop once hit the n2 if just one 2 is available otherwise gonna stop till the last 2
+// console.log(number.toFixed(7));  //it will fill the difference with 0s
+// console.log(number.toFixed());   // it will give the integer
+
+// END toFixed
+
+
+// VALUE OF
+
+// var b ="abcd";
+// var c = b.valueOf();
+
+// console.log(c);
+
+// END VALUE OF
+
+
+//TO PRECISION
+
+var number = 8.1234
+console.log(number.toPrecision(3));
+
+
+//END TO PRECISION
+
